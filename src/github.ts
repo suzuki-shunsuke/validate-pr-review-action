3028e24b517bd70cd2617b06e6ec39aa8b4501e9
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-07-18 07:52:51 +0900

    fix: fix a bug that validation fails while pagination (#168)
    * fix: fix a bug that validation fails while pagination
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/github.ts
+++ src/github.ts
@@ -1,11 +1,15 @@
 import * as github from "@actions/github";
 import * as lib from "./lib";
+import * as types from "./type";
 import { Octokit } from "@octokit/core";
 import { paginateGraphQL } from "@octokit/plugin-paginate-graphql";
+import { z } from "zod";
 
-export const getPullRequest = async (input: lib.Input): Promise<any> => {
+export const getPullRequest = async (
+  input: lib.Input,
+): Promise<types.PullRequest> => {
   const octokit = github.getOctokit(input.githubToken);
-  return await octokit.graphql<any>(
+  const result = await octokit.graphql<any>(
     `query($owner: String!, $repo: String!, $pr: Int!) {
   repository(owner: $owner, name: $repo) {
     pullRequest(number: $pr) {
@@ -64,12 +68,26 @@ export const getPullRequest = async (input: lib.Input): Promise<any> => {
       pr: input.pullRequestNumber,
     },
   );
+  return types.PullRequest.parse(result);
 };
 
-export const listCommits = async (input: lib.Input): Promise<any> => {
+const QueryCommits = z.object({
+  repository: z.object({
+    pullRequest: z.object({
+      commits: types.Commits,
+    }),
+  }),
+});
+
+type QueryCommits = z.infer<typeof QueryCommits>;
+
+export const listCommits = async (
+  input: lib.Input,
+  cursor: string,
+): Promise<types.PullRequestCommit[]> => {
   const MyOctokit = Octokit.plugin(paginateGraphQL);
   const octokit = new MyOctokit({ auth: input.githubToken });
-  return await octokit.graphql.paginate(
+  const result = await octokit.graphql.paginate(
     `query paginate($owner: String!, $repo: String!, $pr: Int!, $cursor: String) {
   repository(owner: $owner, name: $repo) {
     pullRequest(number: $pr) {
@@ -102,19 +120,34 @@ export const listCommits = async (input: lib.Input): Promise<any> => {
       owner: input.repositoryOwner,
       repo: input.repositoryName,
       pr: input.pullRequestNumber,
+      cursor: cursor,
     },
   );
+  const pr = QueryCommits.parse(result);
+  return pr.repository.pullRequest.commits.nodes;
 };
 
-export const listReviews = async (input: lib.Input): Promise<any> => {
+const QueryReviews = z.object({
+  repository: z.object({
+    pullRequest: z.object({
+      reviews: types.Reviews,
+    }),
+  }),
+});
+
+type QueryReviews = z.infer<typeof QueryReviews>;
+
+export const listReviews = async (
+  input: lib.Input,
+  cursor: string,
+): Promise<types.Review[]> => {
   const MyOctokit = Octokit.plugin(paginateGraphQL);
   const octokit = new MyOctokit({ auth: input.githubToken });
-  return await octokit.graphql.paginate(
+  const result = await octokit.graphql.paginate(
     `query paginate($owner: String!, $repo: String!, $pr: Int!, $cursor: String) {
   repository(owner: $owner, name: $repo) {
     pullRequest(number: $pr) {
       reviews(first: 100, after: $cursor) {
-        totalCount
         pageInfo {
           hasNextPage
           endCursor
@@ -137,6 +170,9 @@ export const listReviews = async (input: lib.Input): Promise<any> => {
       owner: input.repositoryOwner,
       repo: input.repositoryName,
       pr: input.pullRequestNumber,
+      cursor: cursor,
     },
   );
+  const pr = QueryReviews.parse(result);
+  return pr.repository.pullRequest.reviews.nodes;
 };
